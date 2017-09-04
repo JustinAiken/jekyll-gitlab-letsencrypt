21ef31a9c0d63b6573a13380d060f3fffafe0f26
Justin Aiken <60tonangel@gmail.com>
2017-09-04 16:00:21 -0600

    Bump to version 0.0.2

---
+++ CHANGELOG.markdown
@@ -0,0 +1,7 @@
+# 0.0.2
+
+- #3 - Added option to specify scheme used for challenge url request
+
+# 0.0.1
+
+- Initial Release
--- lib/jekyll/gitlab/letsencrypt/version.rb
+++ lib/jekyll/gitlab/letsencrypt/version.rb
@@ -1,7 +1,7 @@
 module Jekyll
   module Gitlab
     module Letsencrypt
-      VERSION = "0.0.1"
+      VERSION = "0.0.2"
     end
   end
 end
