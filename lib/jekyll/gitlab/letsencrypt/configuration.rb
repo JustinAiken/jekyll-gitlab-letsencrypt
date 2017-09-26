5a929ce28f6ae70ae488fd0533c46ac2732fbcbe
Justin Aiken <60tonangel@gmail.com>
2017-09-26 17:01:17 -0600

    Make it possible to pass token from ENV

--- lib/jekyll/gitlab/letsencrypt/configuration.rb
+++ lib/jekyll/gitlab/letsencrypt/configuration.rb
@@ -11,12 +11,12 @@ module Jekyll
         DEFAULT_DELAY_TIME      = 15
         DEFAULT_SCHEME          = 'http'
 
-        REQUIRED_KEYS = %w{gitlab_repo personal_access_token email domain}
+        REQUIRED_KEYS = %w{gitlab_repo email domain}
 
         class << self
 
           def valid?
-            REQUIRED_KEYS.all? { |key| jekyll_config.has_key? key }
+            REQUIRED_KEYS.all? { |key| jekyll_config.has_key? key } && personal_access_token
           end
 
           def endpoint
@@ -40,7 +40,7 @@ module Jekyll
           end
 
           def personal_access_token
-            jekyll_config['personal_access_token']
+            ENV['GITLAB_TOKEN'] || jekyll_config['personal_access_token']
           end
 
           def email
